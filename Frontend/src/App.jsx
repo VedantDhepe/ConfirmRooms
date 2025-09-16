e0017d3c0dc55577c92f15d700071c2e6099738c
Vedant Dhepe <vedantdhepe101@gmail.com>
2025-09-17 01:40:41 +0530

    Removed unwanted imports from App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect, useContext } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
 import NewListing from './pages/NewListing'
